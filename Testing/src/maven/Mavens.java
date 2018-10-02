4b29bd59ee5bb4285d0ecba4838dc01513d2b819
v.bamdeo.sharma <v.bamdeo.sharma@BDC8-LX-1GZ7152.dir.svc.accenture.com>
2018-10-02 12:43:36 +0530

    sa

--- Testing/src/maven/Mavens.java
+++ Testing/src/maven/Mavens.java
@@ -3,6 +3,8 @@ package maven;
 // We can add dependencies from maven repository i.e mvnrepository.com
 //Add maven project from help -> install new software
 //Artifact Id is project name
+// Acommand to run maven is mvn clean test we have to be in cucumber project
+
 public class Mavens {
 
 	public static void main(String[] args) {
